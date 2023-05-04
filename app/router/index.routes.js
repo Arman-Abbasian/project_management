9796320b765454af2c25049c92ad26d444b4e029
Arman Abbasian <abasian.arman@gmail.com>
2023-05-04 13:33:48 +0430

    make all routes for project

--- app/router/index.routes.js
+++ app/router/index.routes.js
@@ -1,10 +1,19 @@
 const {Router}=require("express");
+const authRoutes = require("./auth.routes");
+const projectRoutes = require("./project.routes");
+const teamRoutes = require("./team.routes");
+const userRoutes = require("./user.routes");
 const router=Router();
 router.get("/",(req,res,next)=>{
     return res.json({
         message:"this is a new route"
     })
 })
+router.use("/auth",authRoutes);
+router.use("/project",projectRoutes);
+router.use("/team",teamRoutes);
+router.use("/user",userRoutes)
+
 module.exports={
     AllRoutes:router
 }
